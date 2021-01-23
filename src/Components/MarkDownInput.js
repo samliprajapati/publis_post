84e73b4b68e530ba29ab37b5ea6f2c7beb700f41
samliprajapati <samliprajapati9@gmail.com>
2021-01-23 18:48:55 +0530

    Markdown editor

---
+++ src/Components/MarkDownInput.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  border: 1px solid gainsboro;
+  width: 100%;
+  height: 80px;
+  margin: 10px 0px;
+  &&:focus {
+    outline: none;
+  }
+  &&:active {
+    outline: none;
+  }
+`;
+
+export function MarkedDownInput(props) {
+  console.log(props.handleBody);
+  return <TextArea placeholder={"Body"} onChange={props.handleBody} />;
+}
---
+++ src/Components/SearchInputBox.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import SearchInpuBox from "./SearchInputBox";
+describe("SearchInputBox", () => {
+  it("hello", () => {
+    render(<SearchInpuBox />);
+  });
+});
