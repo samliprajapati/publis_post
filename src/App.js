c1d929a157980e211e21d654b89985a8696e5c23
samliprajapati <samliprajapati9@gmail.com>
2021-01-23 18:47:48 +0530

    Search Function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SearchInpuBox from "./Components/SearchInputBox";
+import styled from "styled-components";
+import { useMemo, useReducer, useState } from "react";
+import NewAndPublishedButton from "./Components/NewAndPublishedButton";
+import Published from "./Components/PublishedPost";
+import NewPostForm from "./Components/NewPostForm";
+import { act } from "react-dom/test-utils";
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const MainContainer = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 50px;
+`;
+
+const initialState = [];
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload];
+    default:
+      return [...state];
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [inputData, setInputData] = useState("");
+  const [ButtonData, setbuttonData] = useState("Published");
+  const [titleData, setTitle] = useState("");
+  const [bodyData, setBody] = useState("");
+  const [validation, setValidation] = useState(false);
+
+  function handleInputChange(e) {
+    setInputData(e.target.value);
+  }
+  function handleTitle(e) {
+    setTitle(e.target.value);
+  }
+  function handleBody(e) {
+    const newValue = e.currentTarget.value;
+    setBody(newValue);
+  }
+  function handleClear() {
+    setInputData("");
+  }
+  function handleButton(data) {
+    setbuttonData(data);
+  }
+
+  function handlePost() {
+    if (titleData === "" && bodyData === "") {
+      setValidation(true);
+    } else {
+      dispatch({
+        type: "add",
+        payload: { title: titleData, body: bodyData },
+      });
+      setTitle("");
+      setBody("");
+      alert("success Fully create new post");
+      setbuttonData("Published");
+    }
+  }
+
+  const filteredPublished = useMemo(() => {
+    return inputData === ""
+      ? state
+      : state.filter((item) => {
+          debugger;
+          return (
+            item.title.toLowerCase().includes(inputData.toLowerCase()) ||
+            item.body.toLowerCase().includes(inputData.toLowerCase())
+          );
+        });
+  }, [inputData, state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Container>
+        {" "}
+        <SearchInpuBox
+          handleInputChange={handleInputChange}
+          handleClear={handleClear}
+          inputData={inputData}
+        />
+      </Container>
+      <Container>
+        <NewAndPublishedButton
+          handleButton={handleButton}
+          ButtonData={ButtonData}
+        />
+      </Container>
+      <Container>
+        {ButtonData === "Published" ? (
+          <Published state={filteredPublished} />
+        ) : (
+          <NewPostForm
+            dispatch={dispatch}
+            handleTitle={handleTitle}
+            titleData={titleData}
+            bodyData={bodyData}
+            handleBody={handleBody}
+            handlePost={handlePost}
+            validation={validation}
+          />
+        )}
+      </Container>
+    </MainContainer>
   );
 }
 
